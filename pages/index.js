6a1c430d3863df062c37a24e1d4a17f458b3c2e0
Marçal Junior <marssaljr@gmail.com>
2021-07-14 12:11:32 -0300

    feat: create form

--- pages/index.js
+++ pages/index.js
@@ -32,28 +32,67 @@ export default function Home() {
     <>
     <AlurakutMenu/>
     <MainGrid>
-      <div className="profileArea" style={{ gridArea: 'profileArea'}}>
+      <div 
+        className="profileArea"
+        style={{ gridArea: 'profileArea'}}>
         <ProfileSideBar user={githubuser}/>
       </div>
-      <div className="postsArea" style={{ gridArea: 'postsArea'}}>
+      <div
+        className="postsArea"
+        style={{ gridArea: 'postsArea'}}>
         <Box>
-          <h1 className="title">
+          <h1
+           className="title">
             Bem-vindo(a) {githubuser}
           </h1>
 
           <OrkutNostalgicIconSet/>
         </Box>
+        <Box>
+          <h2 className="subTitle">O que você deseja fazer?</h2>
+          <form>
+            <div>
+            <input
+             placeholder="Qual vai ser o nome da sua comunidade?"
+             name="title"
+             aria-label="Qual vai ser o nome da sua comunidade?"
+             type="text"
+            />
+            </div>
+            <div>
+            <input
+             placeholder="Coloque uma URL para usar de capa"
+             name="image"
+             aria-label="Qual vai ser o nome da sua comunidade?"
+            />
+            </div>
+            <button>
+              Criar comunidade
+            </button>
+          </form>
+        </Box>
       </div>
-      <div className="profileRelationsArea"  style={{gridArea: 'profileRelationsArea'}}>
+      <div
+       className="profileRelationsArea"
+       style={{gridArea: 'profileRelationsArea'}}>
         <ProfileRelationsBoxWrapper>
-          <h2 className="smallTitle">Pessoas da comunidade ({peopleWhoLiked.length})</h2>
+          <h2 className="smallTitle">
+            Pessoas da comunidade ({peopleWhoLiked.length})
+          </h2>
           <ul>
-            {peopleWhoLiked.map((people) => {
+            {peopleWhoLiked.map(
+              (people) => {
               return (
                 <li>
-                  <a href={`/users/${people}`} key={people}>
-                    <img src={`https://github.com/${people}.png`} alt={people} />
-                    <span>{people}</span>
+                  <a
+                   href={`/users/${people}`}
+                   key={people}>
+                    <img
+                     src={`https://github.com/${people}.png`} 
+                     alt={people} />
+                    <span>
+                      {people}
+                    </span>
                   </a>
                 </li>
               )
